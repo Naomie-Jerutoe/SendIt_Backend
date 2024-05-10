8c9e8e14b80bd5774eeb2ec45453aef89f3fb89f
Naomi Lagat <naomiejerutoe@gmail.com>
2024-05-10 16:26:45 +0300

    feat: Implement Profile Picture Removal Endpoint

--- routes/courier_bp.py
+++ routes/courier_bp.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, make_response, jsonify, request
-from flask_restful import Api, Resource, abort
+from flask_restful import Api, Resource
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from schemas import profileSchema
@@ -7,7 +7,7 @@ import cloudinary
 import cloudinary.uploader
 
 from auth_middleware import admin_required
-from models import db, Profile
+from models import db, Profile, User
 
 profile_bp = Blueprint('profile_bp', __name__)
 ma = Marshmallow(profile_bp)
@@ -69,3 +69,26 @@ class UploadProfilePicture(Resource):
             return make_response(jsonify({"error": str(e)}), 500)
 
 api.add_resource(UploadProfilePicture, '/upload_profile_picture')
+
+class RemoveProfilePicture(Resource):
+  @jwt_required()
+  def delete(self, profile_id):
+    user_id = get_jwt_identity()
+    profile = Profile.query.get_or_404(profile_id)
+    
+    # Check if the requesting user is the owner of the profile or an admin
+    current_user = User.query.get(user_id)
+    if profile.user_id != user_id and not current_user.is_admin:
+        return jsonify({'error': 'You are not authorized to modify this profile.'}), 403
+    
+    if profile.profile_picture:
+            public_id = cloudinary.uploader.get_public_id(profile.profile_picture)
+            cloudinary.uploader.destroy(public_id)
+            
+    # Remove the profile picture URL from the database
+    profile.profile_picture = None
+    db.session.commit()
+    
+    return make_response(jsonify({'message': 'Profile picture removed successfully.'}), 200)
+
+api.add_resource(RemoveProfilePicture, '/remove_profile_picture/<string:id>')
