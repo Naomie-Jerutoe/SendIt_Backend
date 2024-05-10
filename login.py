cfdf8d7cf269a3976fca7edad1654c40c157b1fa
Levis Ngigi <ngigilevis@gmail.com>
2024-05-10 13:41:31 +0300

    feat:add logout route

--- login.py
+++ login.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request, jsonify
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt, get_jwt_identity
 from sqlalchemy.exc import IntegrityError
-from models import db, User, Parcel
+from models import db, User, TokenBlocklist
+from auth_middleware import admin_required
 from datetime import datetime
 
 users_bp = Blueprint('users', __name__)
@@ -11,12 +12,23 @@ bcrypt = Bcrypt()
 @users_bp.route('/signup', methods=['POST'])
 def signup():
     data = request.get_json()
+
+    email = data.get('email')
+    password = data.get('password')
+    
+    
+    if not email or '@' not in email or '.' not in email:
+        return jsonify({"error": "Invalid email address."}), 400
+
+    if not (6 <= len(password) <= 50):
+        return jsonify({"error": "Password must be between 8 and 50 characters."}), 400
+
     hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
 
     new_user = User(
         email=data['email'],
         username=data['username'],
-        is_admin=False,
+        is_admin=data['is_admin'],
         password=hashed_password
     )
 
@@ -40,17 +52,19 @@ def login():
         return jsonify({'message': 'The credentials you provided are incorrect'}), 400
 
 
-@users_bp.route('/users/<int:user_id>/makeadmin', methods=['PATCH'])
+@users_bp.route('/makeadmin', methods=['GET'])
+@admin_required
+def make_admin():
+    return jsonify({'message': 'Admin registered successfully'})
+
+
+@users_bp.route('/logout', methods=['GET'])
 @jwt_required()
-def make_user_admin(user_id):
-    current_user = get_jwt_identity()
-    if current_user['is_admin']:
-        user = User.query.get(user_id)
-        if user:
-            user.is_admin = True
-            db.session.commit()
-            return jsonify({'message': 'The user has been made an admin successfully'}), 200
-        else:
-            return jsonify({'message': 'No such user'}), 204
-    else:
-        return jsonify({'message': 'Only admins can access this route'}), 403
+def logout():
+    current_user_data = get_jwt_identity()
+    token = get_jwt()
+    blocked_token = TokenBlocklist(
+        jti=token['jti'], created_at=datetime.utcnow())
+    db.session.add(blocked_token)
+    db.session.commit()
+    return jsonify({'detail': "Logged out Successfully"})
