520a5de390c2db48117fcd3b678bf88accbfe2f2
Naomi Lagat <naomiejerutoe@gmail.com>
2024-05-10 15:51:17 +0300

    feat: register profile_bp and add cloudinary configurations

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from datetime import timedelta
 import os
+from routes.courier_bp import profile_bp
 
-from models import db, User, Parcel, Order, Profile
+from models import db, User, Parcel, Order, Profile, TokenBlocklist
+from routes.courier_bp import profile_bp
 
 def create_app():
     app = Flask(__name__)
@@ -18,11 +20,17 @@ def create_app():
     app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
     
+    app.config['CLOUDINARY_CLOUD_NAME'] = os.getenv('CLOUDINARY_CLOUD_NAME')
+    app.config['CLOUDINARY_API_KEY'] = os.getenv('CLOUDINARY_API_KEY')
+    app.config['CLOUDINARY_API_SECRET'] = os.getenv('CLOUDINARY_API_SECRET')
+    
 
     migrate = Migrate(app, db)
     db.init_app(app)
     JWTManager(app)
     CORS(app)
+    
+    app.register_blueprint(profile_bp)
     
     return app
 
