6c7dd77e02193aeb2a66b5b9fc9dfef6ce164799
bryokn <bryokn@gmail.com>
2024-05-12 13:04:12 +0300

    added flask mail config in app and mail notif in order_bp

--- routes/order_bp.py
+++ routes/order_bp.py
@@ -3,17 +3,18 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from marshmallow import ValidationError
 from models import Order, db, User
 from schemas import OrderSchema, orderSchema
-from auth_middleware import admin_required  #Import the admin_required decorator
+#from auth_middleware import admin_required  #Import the admin_required decorator
+from app import mail
 
 # Create a Blueprint for order-related routes with the prefix '/api/v1/orders'
 orders_bp = Blueprint('orders', __name__, url_prefix='/api/v1/orders')
 
 @orders_bp.route('/', methods=['POST'])
-@jwt_required()
+#@jwt_required()
 def create_order():
     try:
         data = request.get_json()
-        current_user_id = get_jwt_identity()
+        #current_user_id = get_jwt_identity()
         order_data = {
             'parcel': data['parcel'],
             'status': 'enroute'
@@ -26,8 +27,8 @@ def create_order():
         return jsonify({'error': err.messages}), 400
 
 @orders_bp.route('/<int:order_id>', methods=['PUT'])
-@jwt_required()
-@admin_required  # Decorate the function with the admin_required decorator
+#@jwt_required()
+#@admin_required  # Decorate the function with the admin_required decorator
 def update_order(order_id):
     try:
         data = request.get_json()
@@ -37,7 +38,15 @@ def update_order(order_id):
 
         # Update the order status if provided in the request data
         if 'status' in data:
+            previous_status = order.status
             order.status = data['status']
+            #get the user email associated with the order
+            user = User.query.get(order.user_id)
+            if user:
+                #send email notification to the user
+                msg = Message('Order Status Updated', recipients=[user.email])
+                msg.body = f"Your order {order.id} status has been changed from '{previous_status}' to '{order.status}'."
+                mail.send(msg)
 
         db.session.commit()
 
