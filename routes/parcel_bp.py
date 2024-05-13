fa1e69fb375debaa6a9823424d7b6b26d2430725
Brian <brianmariga0@gmail.com>
2024-05-13 10:33:42 +0300

    feat: Parcel routes done

--- routes/parcel_bp.py
+++ routes/parcel_bp.py
@@ -68,7 +68,7 @@ class ViewAndCreateParcel(Resource):
 # Route 2: Get details of a parcel order by parcel ID
 # @parcel_bp.route('/parcels/<int:parcel_id>', methods=['GET'])
 class GetParcel(Resource):
-    @jwt_required
+    @jwt_required()
     def get(self, parcel_id):
         parcel = Parcel.query.filter_by(id=parcel_id).first()
         if parcel:
@@ -80,7 +80,7 @@ class GetParcel(Resource):
 # Route 3: Change destination of a specific parcel delivery order
 # @parcel_bp.route('/parcels/<int:parcel_id>/destination', methods=['PUT'])
 class ChangeRoute(Resource):
-    @jwt_required
+    @jwt_required()
     def put(self, parcel_id):
         user_id = get_jwt_identity()
         parcel = Parcel.query.get(parcel_id)
@@ -91,7 +91,6 @@ class ChangeRoute(Resource):
             return make_response(jsonify({"message": "Destination updated successfully! Your parcel is on its way to the new destination.", "parcel": parcel.__repr__()}), 200)
         else:
             return make_response(jsonify({"message": "Sorry, we can't change the destination of this parcel."}), 400)
-
 
 # # Route 4: Cancel a specific parcel delivery order
 # @parcel_bp.route('/parcels/<int:parcel_id>/cancel', methods=['PUT'])
@@ -111,8 +110,11 @@ class CancelParcel(Resource):
 # # Route 5: Get a list of all parcel deliveries associated to a user
 # @parcel_bp.route('/users/<int:user_id>/parcels', methods=['GET'])
 class GetUserParcels(Resource):
-    @jwt_required
+    @jwt_required()
     def get(self, user_id):
+        if user_id is None:
+            return make_response(jsonify({"message": "User ID not found in token."}), 403)
+        
         parcels = Parcel.query.filter_by(user_id=user_id).all()
         return make_response(jsonify({"message": "Here are all the parcels associated with this user!", "parcels": [parcel.__repr__() for parcel in parcels]}), 200)
 
