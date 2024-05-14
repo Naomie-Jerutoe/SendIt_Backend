479db7aa24e74cea534a43470d77bb25d062929c
Dennis <denkipyator@gmail.com>
2024-05-14 18:08:28 +0300

    user_bp_test.py/Testing getting user by id

---
+++ tests/user_bp_test.py
@@ -0,0 +1,191 @@
+try:
+    import requests
+
+    response = requests.get('http://localhost:5000/users')
+
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "email": "John@email.com",
+            "id": 5,
+            "is_admin": False,
+            "username": "davidjohn"
+        },
+        {
+            "email": "sivel123@email.com",
+            "id": 6,
+            "is_admin": True,
+            "username": "sivel123"
+        },
+        {
+            "email": "Johnsivel@email.com",
+            "id": 7,
+            "is_admin": True,
+            "username": "oscar"
+        },
+        {
+            "email": "jamesdavid@email.com",
+            "id": 8,
+            "is_admin": True,
+            "username": "davidjames"
+        },
+        {
+            "email": "jonathan@gmail.com",
+            "id": 9,
+            "is_admin": True,
+            "username": "Jonathan Kim"
+        },
+        {
+            "email": "davioscar@email.com",
+            "id": 11,
+            "is_admin": False,
+            "username": "muchiri"
+        },
+        {
+            "email": "felista@email.com",
+            "id": 13,
+            "is_admin": True,
+            "username": "felista"
+        },
+        {
+            "email": "george@email.com",
+            "id": 12,
+            "is_admin": True,
+            "username": "george"
+        },
+        {
+            "email": "levis@gmail.com",
+            "id": 14,
+            "is_admin": False,
+            "username": "Levis Ngigi"
+        },
+        {
+            "email": "dennis@gmail.com",
+            "id": 16,
+            "is_admin": False,
+            "username": "Dennis Ngigi"
+        },
+        {
+            "email": "naomi@gmail.com",
+            "id": 17,
+            "is_admin": True,
+            "username": "Naomie Jeruto"
+        },
+        {
+            "email": "collins@gmail.com",
+            "id": 18,
+            "is_admin": False,
+            "username": "Collins Bett"
+        },
+        {
+            "email": "jane@gmail.com",
+            "id": 19,
+            "is_admin": False,
+            "username": "Jane Doe"
+        },
+        {
+            "email": "brian@gmail.com",
+            "id": 20,
+            "is_admin": True,
+            "username": "brian"
+        },
+        {
+            "email": "johnkip@gmail.com",
+            "id": 23,
+            "is_admin": True,
+            "username": "John Kip"
+        },
+        {
+            "email": "naomicherutolagat@gmail.com",
+            "id": 24,
+            "is_admin": False,
+            "username": "naomi lagat"
+        },
+        {
+            "email": "john@example.com",
+            "id": 1,
+            "is_admin": True,
+            "username": "Johndoe"
+        },
+        {
+            "email": "joe@gmail.com",
+            "id": 25,
+            "is_admin": True,
+            "username": "joe"
+        },
+        {
+            "email": "kipkirui@gmail.com",
+            "id": 26,
+            "is_admin": False,
+            "username": "kipkirui"
+        },
+        {
+            "email": "kip@gmail.com",
+            "id": 27,
+            "is_admin": True,
+            "username": "kip"
+        },
+        {
+            "email": "max@gmail.com",
+            "id": 28,
+            "is_admin": True,
+            "username": "max"
+        },
+        {
+            "email": "may@gmail.com",
+            "id": 30,
+            "is_admin": False,
+            "username": "may"
+        },
+        {
+            "email": "mat@gmail.com",
+            "id": 32,
+            "is_admin": True,
+            "username": "mat"
+        },
+        {
+            "email": "bryo@gmail.com",
+            "id": 34,
+            "is_admin": True,
+            "username": "bryo"
+        },
+        {
+            "email": "bryokm@gmail.com",
+            "id": 35,
+            "is_admin": False,
+            "username": "Brian K"
+        },
+        {
+            "email": "sirbrian8@gmail.com",
+            "id": 36,
+            "is_admin": False,
+            "username": "sirbrian"
+        },
+        {
+            "email": "sirbryo@gmail.com",
+            "id": 37,
+            "is_admin": True,
+            "username": "sirbryo"
+        }
+    ]
+    print('-----------------------------------GOT ALL USERS SUCCESSFULLY-----------------------------------------')
+except Exception:
+    print("-------------------------------------GETTING ALL USERS FAILED-----------------------------")
+
+try:
+    import requests
+    user_id = 5 
+    response = requests.get(f'http://localhost:5000/users/{user_id}')
+
+    assert response.status_code == 200
+    print (response.json())
+    assert response.json() == {
+            "email": "John@email.com",
+            "id": 5,
+            "is_admin": False,
+            "username": "davidjohn"
+        }
+    print('-----------------------------------GOT USER 5 SUCCESSFULLY-----------------------------------------')
+except Exception:
+    print("-------------------------------------GETTING USER 5 FAILED-----------------------------")
+   
